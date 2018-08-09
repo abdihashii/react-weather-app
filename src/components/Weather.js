7b6d5e6e92835fc628c76ac62f681b90fd82939d
Abdirahman Haji <hajix007@umn.edu>
2018-08-08 20:53:00 -0700

    Finishing styling w/ bootstrap, finished part 2 of tut

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,12 +9,38 @@ const Weather = props => (
   // can only one single element if you use 'return', or you can return as much
   // as you'd like by removing 'return' and the curly braces and replace it with
   // parens
-  <div>
-    { props.temperature && <p>Temperature: {props.temperature}</p> }
-    { props.city && props.country && <p>Location: {props.city}, { props.country}</p> }
-    { props.humidity && <p>Humidity: {props.humidity}</p> }
-    { props.description && <p>Conditions: {props.description}</p> }
-    { props.error && <p>{props.error}</p> }
+  <div className="weather__info">
+    {
+      props.temperature &&
+      <p className="weather__key">Temperature:
+        <span className="weather__value">{props.temperature}</span>
+      </p>
+    }
+    {
+      props.city &&
+      props.country &&
+      <p className="weather__key">Location:
+        <span className="weather__value">{props.city}, { props.country}</span>
+      </p>
+    }
+    {
+      props.humidity &&
+      <p className="weather__key">Humidity:
+        <span className="weather__value"> {props.humidity}</span>
+      </p>
+    }
+    {
+      props.description &&
+      <p className="weather__key">Conditions:
+        <span className="weather__value"> {props.description}</span>
+      </p>
+    }
+    {
+      props.error &&
+      <p>
+        {props.error}
+      </p>
+    }
   </div>
 );
 
