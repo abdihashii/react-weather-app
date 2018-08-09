16e19b8b785c6831ac33fd7dd22139bea2ecb675
Abdirahman Haji <hajix007@umn.edu>
2018-08-08 21:43:02 -0700

    Spell check temperature

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from "react";
 const Titles = () => (
   <div>
       <h1 className="title-container__title">Weather Finder</h1>
-      <p className="subtitle-container__title">Find out temprature, conditions, and more...</p>
+      <p className="subtitle-container__title">Find out temperature, conditions, and more...</p>
   </div>
 );
 
