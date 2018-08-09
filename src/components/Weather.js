ec9f2086bdde226489672c3b33526ce9e9451ea9
Abdirahman Haji <hajix007@umn.edu>
2018-08-08 19:30:42 -0700

    Finishing part 1 of video

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,21 @@
 import React from "react";
-
-class Weather extends React.Component {
-    render() {
-        return (
-            <div>
-                { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
-                { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-                { this.props.description && <p>Conditions: {this.props.description}</p> }
-                { this.props.error && <p>{this.props.error}</p> }
-            </div>
-        );
-    }
-};
+/*
+ Stateless functional component that doesn't contain any states, just props,
+ This component doesn't need the other functionality such as the App.js comp
+ Need to pass in 'props' argument in order to call the props from App.js
+ can also remove the parens from '(props)' if only returning one
+*/
+const Weather = props => (
+  // can only one single element if you use 'return', or you can return as much
+  // as you'd like by removing 'return' and the curly braces and replace it with
+  // parens
+  <div>
+    { props.temperature && <p>Temperature: {props.temperature}</p> }
+    { props.city && props.country && <p>Location: {props.city}, { props.country}</p> }
+    { props.humidity && <p>Humidity: {props.humidity}</p> }
+    { props.description && <p>Conditions: {props.description}</p> }
+    { props.error && <p>{props.error}</p> }
+  </div>
+);
 
 export default Weather;
